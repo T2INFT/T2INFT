521f1184dbab850ed30a8577b4634a399d323fc9
kirigiricloud <kirigiricloud@gmail.com>
2023-07-10 12:35:54 +0800

    edited main py scripts

--- main-mixer.py
+++ main-mixer.py
@@ -3,6 +3,17 @@ import subprocess
 import sys
 import shutil
 
+def find_last(source_dir):
+    work_space = os.getcwd()
+    source_dir = os.path.join(work_space,source_dir)
+    index = 0
+    for root, dirs, files in os.walk(source_dir):
+        for file in files:
+            if file.endswith(".png"):
+                index += 1
+    return index
+
+
 user_name = sys.argv[1]
 
 if os.path.isdir('outputs'):
@@ -21,25 +32,32 @@ else:
 if len(sys.argv)>2:
     prompt = sys.argv[2]
     working_dir = os.getcwd()
-    sd_image_in = os.path.join(working_dir,'sd-outputs',user_name,'sd-'+user_name+'.png')
+    sd_in_dir = os.path.join(working_dir,'sd-outputs',user_name)
+    sd_image_in = os.path.join(sd_in_dir,'sd-'+user_name+'-'+str(find_last(sd_in_dir)-1)+'.png')
     sd_image_out = os.path.join(working_dir,'stable-diffusion','input','input.png')
     shutil.copyfile(sd_image_in, sd_image_out)
+    if os.path.isfile(sd_image_in):
+        print('successfully copied image')
 prev_path = os.getcwd()
 os.chdir(prev_path+'/stable-diffusion')
-print(os.getcwd())
+#print(os.getcwd())
 subprocess.run(["python", "scripts/mixer-test.py",user_name])
 os.chdir(prev_path)
-print(os.getcwd())
-
-
+#print(os.getcwd())
 image_out = os.getcwd()+'/stable-diffusion/output/'+user_name+'.png'
-image_new_out = os.getcwd()+'/outputs/'+user_name+'/'+user_name+'.png'
+mixer_out_dir = os.path.join(os.getcwd(),'outputs',user_name)
+image_new_out = os.path.join(mixer_out_dir,user_name+'-'+str(find_last(mixer_out_dir))+'.png')
 print(image_out)
+f = open("mixer-log.txt", "w")
 if os.path.isfile(image_out):
-    print('get')
+    print('successfully transfered image')
     os.rename(image_out, image_new_out)
+    ##save out_path in a text file
+    f.write(image_new_out)
+    
 else:
-    print('not get')
+    print('image not found')
 
-print(user_name+'.png')
-#print(os.path.isfile('outputs\\'+user_name+'.jpg'))
+f.close()
+
+    
